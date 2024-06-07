a6a63f93fde088968f8122335fca9ae09316a217
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-06-07 14:24:53 -0400

    Implement Both Sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,38 +1,46 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSidebar from '@/components/RightSidebar';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = {firstName: 'Hillary', lastName: 'Gilbert', email: 'user@hillarygilbert.com'};
+  const loggedIn = {
+    firstName: "Hillary",
+    lastName: "Gilbert",
+    email: "user@hillarygilbert.com",
+  };
 
   return (
-    <section className='home'>
-    <div className='home-content'>
-      <header className="home-header">
-       <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || 'Guest'}
-          subtext="Easily access and manage your account and transactions."
-       />
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Easily access and manage your account and transactions."
+          />
 
-       <TotalBalanceBox
-        accounts={[]}
-        totalBanks={1}
-        totalCurrentBalance={15250.16}
-       />
-      </header>
-
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={15250.16}
+          />
+        </header>
         RECENT TRANSACTIONS
-    </div>
+      </div>
 
-    <RightSidebar
-      user={loggedIn}
-      transactions={[]}
-      banks={[{}, {}]}
-    />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[
+          { currentBalance: 8124.9 },
+          {
+            currentBalance: 5320.5,
+          },
+        ]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import BankCard from './BankCard'
 //RightSidebar  with destructured props
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
@@ -49,12 +50,22 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
           {/* Show the first bank card */}
           <div className='relative z-10'>
-            BANK CARD 1
+            <BankCard
+            key={banks[0].$id}
+            account={banks[0]}
+            userName={`${user.firstName} ${user.lastName}`}
+            showBalance={false}
+            />
           </div>
             {/* Show second bank card if it exists */}
           {banks [1] && (
             <div className='absolute right-0 top-8 z-0 w-[90%]'>
-              BANK CARD 2
+              <BankCard
+              key={banks[1].$id}
+              account={banks[1]}
+              userName={`${user.firstName} ${user.lastName}`}
+              showBalance={false}
+              />
 
             </div>
           )}
